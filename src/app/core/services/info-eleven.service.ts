b032d0633b45b6f9e6f5d69795f1b63598e87010
Monise da Silva <monisesilva91@hotmail.com>
2023-09-30 19:14:11 -0300

    Chamando a API da ElevenLab que transforma texto em audio

--- src/app/core/services/info-eleven.service.ts
+++ src/app/core/services/info-eleven.service.ts
@@ -21,10 +21,30 @@ export class InfoElevenService {
     });
   }
 
+  public postTextToSpeech(texto: any) {
+    const idVoz = "21m00Tcm4TlvDq8ikWAM";
+    const idModelo = "eleven_multilingual_v1";
+    const idIdioma = "pt";
 
+    const apiUrl = `https://api.elevenlabs.io/v1/text-to-speech/${idVoz}`;
 
+    const headers = {
+      'Content-Type': 'application/json',
+      'xi-api-key': '530918fb46da5ee32e6f902e7982e837',
+    };
 
+    const body = {
+      'text': texto,
+      'model_id': idModelo,
+      'voice_settings': {
+        'stability': 0,
+        'similarity_boost': 0,
+        'style': 0,
+        'use_speaker_boost': true,
+        'language_id': idIdioma,
+      },
+    };
 
-
-
+    return axios.post(apiUrl, body, { headers, responseType: 'arraybuffer' });
+  }
 }
